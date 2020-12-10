1c6d43000eec8e43dcc92be4e7970020b6b95767
Motley90 <motley679@gmail.com>
2020-12-10 15:28:51 -0500

    Update PS2Controls.lua

--- PS2Controls.lua
+++ PS2Controls.lua
@@ -4,39 +4,68 @@ local UserControls = {
 };
 
 local UserInput = {
-  pad = Controls.read()
+  GetInput = Controls.read()
   Circle = {
-    Controller = pad:circle(),
+    Remote_Controller = GetInput:circle(),
     Keyboard_Equivalent = { D, F3 } },
   Cross = {
-    Controller = pad:cross(),
+    Remote_Controller = GetInput:cross(),
     Keyboard_Equivalent = { A, F1 } },
   Square = {
-    Controller = pad:square(),
+    Remote_Controller = GetInput:square(),
     Keyboard_Equivalent = { S, F2 } },
   Triangle = {
-    Controller = pad:triangle(),
+    Remote_Controller = GetInput:triangle(),
     Keyboard_Equivalent = { F, F4 } }
+	
+  Up = {
+    Remote_Controller = GetInput:up(),
+    Keyboard_Equivalent = { Up, F5 } },
+  Down = {
+    Remote_Controller = GetInput:down(),
+    Keyboard_Equivalent = { Down, F6 } },
+  Left = {
+    Remote_Controller = GetInput:left(),
+    Keyboard_Equivalent = { Left, F7 } },
+  Right = {
+    Remote_Controller = GetInput:right(),
+    Keyboard_Equivalent = { Right, F8 } }
 };
 
-function Button_Name(ID)
+function Button_Slot(ID)
   if (ID == 0 ) return "Cross";
   if (ID == 1 ) return "Square";
   if (ID == 2 ) return "Circle";
   if (ID == 3 ) return "Triangle";
 end
-				
+
+function Direction_Slot(ID)
+  if (ID == 0 ) return "Up";
+  if (ID == 1 ) return "Down";
+  if (ID == 2 ) return "Left";
+  if (ID == 3 ) return "Right";
+									
 local UserKey = {};
 function onUserKeyPressDown()
-  if (UserInput.Cross.Controller) UserKey.rawset(Button_Name(0), true);
-  if (UserInput.Square.Controller) UserKey.rawset(Button_Name(1), true);
-  if (UserInput.Circle.Controller) UserKey.rawset(Button_Name(2), true);
-  if (UserInput.Triangle.Controller) UserKey.rawset(Button_Name(3), true);
+  if (UserInput.Cross.Controller) UserKey.rawset(Button_Slot(0), true);
+  if (UserInput.Square.Controller) UserKey.rawset(Button_Slot(1), true);
+  if (UserInput.Circle.Controller) UserKey.rawset(Button_Slot(2), true);
+  if (UserInput.Triangle.Controller) UserKey.rawset(Button_Slot(3), true);
+														
+  if (UserInput.Up.Controller) UserKey.rawset(Direction_Slot(0), true);
+  if (UserInput.Down.Controller) UserKey.rawset(Direction_Slot(1), true);
+  if (UserInput.Left.Controller) UserKey.rawset(Direction_Slot(2), true);
+  if (UserInput.Right.Controller) UserKey.rawset(Direction_Slot(3), true);														
 end
 
 function onUserKeyPressUp()
-  if (not UserInput.Cross.Controller) UserKey.rawset(Button_Name(0), false);
-  if (not UserInput.Square.Controller) UserKey.rawset(Button_Name(1), false);
-  if (not UserInput.Circle.Controller) UserKey.rawset(Button_Name(2), false);
-  if (not UserInput.Triangle.Controller) UserKey.rawset(Button_Name(3), false);
+  if (not UserInput.Cross.Controller) UserKey.rawset(Button_Slot(0), false);
+  if (not UserInput.Square.Controller) UserKey.rawset(Button_Slot(1), false);
+  if (not UserInput.Circle.Controller) UserKey.rawset(Button_Slot(2), false);
+  if (not UserInput.Triangle.Controller) UserKey.rawset(Button_Slot(3), false);
+  
+  if (not UserInput.Up.Controller) UserKey.rawset(Direction_Slot(0), false);
+  if (not UserInput.Down.Controller) UserKey.rawset(Direction_Slot(1), false);
+  if (not UserInput.Left.Controller) UserKey.rawset(Direction_Slot(2), false);
+  if (not UserInput.Right.Controller) UserKey.rawset(Direction_Slot(3), false);	
 end
