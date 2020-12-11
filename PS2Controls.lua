d674b1590cc3592d6392ca8cabb78b93055a0b82
Motley90 <motley679@gmail.com>
2020-12-10 23:05:24 -0500

    Update PS2Controls.lua

--- PS2Controls.lua
+++ PS2Controls.lua
@@ -13,47 +13,59 @@ Right = GetInput:right()
 
 function Button_Slot(ID)
 
-  if (ID == 0 ) then return "Cross" end
-  if (ID == 1 ) then return "Square" end
-  if (ID == 2 ) then return "Circle" end
-  if (ID == 3 ) then return "Triangle" end
+  if (ID == 1 ) then return "Cross" end
+  if (ID == 2 ) then return "Square" end
+  if (ID == 3 ) then return "Circle" end
+  if (ID == 4 ) then return "Triangle" end
 end
 
 function Direction_Slot(ID)
-  if (ID == 0 ) then return "Up" end
-  if (ID == 1 ) then return "Down" end
-  if (ID == 2 ) then return "Left" end
-  if (ID == 3 ) then return "Right" end
+  if (ID == 1 ) then return "Up" end
+  if (ID == 2 ) then return "Down" end
+  if (ID == 3 ) then return "Left" end
+  if (ID == 4 ) then return "Right" end
 end
 								
 local UserKey = {}
 function onUserKeyPressDown()
-  if (Cross) then UserKey.rawset(Button_Slot(0), "Controller", true) end
-  if (Square) then UserKey.rawset(Button_Slot(1), "Controller",  true) end
-  if (Circle) then UserKey.rawset(Button_Slot(2), "Controller",  true) end
-  if (Triangle) then UserKey.rawset(Button_Slot(3), "Controller",  true) end
+  if (Cross) then UserKey.insert(Button_Slot(0), "Controller", true) end
+  if (Square) then UserKey.insert(Button_Slot(1), "Controller",  true) end
+  if (Circle) then UserKey.insert(Button_Slot(2), "Controller",  true) end
+  if (Triangle) then UserKey.insert(Button_Slot(3), "Controller",  true) end
 														
-  if (Up) then UserKey.rawset(Direction_Slot(0), "Controller",  true) end
-  if (Down) then UserKey.rawset(Direction_Slot(1), "Controller",  true) end
-  if (Left) then UserKey.rawset(Direction_Slot(2), "Controller",  true) end
-  if (Right) then UserKey.rawset(Direction_Slot(3), "Controller",  true) end
+  if (Up) then UserKey.insert(Direction_Slot(0), "Controller",  true) end
+  if (Down) then UserKey.insert(Direction_Slot(1), "Controller",  true) end
+  if (Left) then UserKey.insert(Direction_Slot(2), "Controller",  true) end
+  if (Right) then UserKey.insert(Direction_Slot(3), "Controller",  true) end
 end
 
 function onUserKeyPressUp()
-  if (Cross == nil) then UserKey.rawset(Button_Slot(0), "Controller",  false) end
-  if (Square == nil) then UserKey.rawset(Button_Slot(1), "Controller",  false) end
-  if (Circle == nil) then UserKey.rawset(Button_Slot(2), "Controller",  false) end
-  if (Triangle == nil) then UserKey.rawset(Button_Slot(3), "Controller",  false) end
+  if (Cross == nil) then UserKey.remove(Button_Slot(0), "Controller") end
+  if (Square == nil) then UserKey.remove(Button_Slot(1), "Controller") end
+  if (Circle == nil) then UserKey.remove(Button_Slot(2), "Controller") end
+  if (Triangle == nil) then UserKey.remove(Button_Slot(3), "Controller") end
   
-  if (Up == nil) then UserKey.rawset(Direction_Slot(0), "Controller",  false) end
-  if (Down == nil) then UserKey.rawset(Direction_Slot(1), "Controller",  false) end
-  if (Left == nil) then UserKey.rawset(Direction_Slot(2), "Controller", false) end
-  if (Right == nil) then UserKey.rawset(Direction_Slot(3), "Controller",  false) end	
+  if (Up == nil) then UserKey.remove(Direction_Slot(0), "Controller") end
+  if (Down == nil) then UserKey.remove(Direction_Slot(1), "Controller") end
+  if (Left == nil) then UserKey.remove(Direction_Slot(2), "Controller") end
+  if (Right == nil) then UserKey.remove(Direction_Slot(3), "Controller") end	
 end
 
-
-while true do
+local idxslot = 4
+local idx = 1
+while true do    
   screen.waitVblankStart()
   onUserKeyPressDown()
   onUserKeyPressUp()
+  local key = UserKey[Button_Slot(idx)]
+   if (key ) 
+   then print(key) 
+   else print(Button_Slot(idx))
+   end
+  
+  if (idx == idxslot) 
+    then idx = 0
+  end	
+    idx = idx + 1
+
 end
